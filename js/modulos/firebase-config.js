d4ab516af386f37f9c2e3095384e73d22967c6e5
Brian Cordero <alfomexgo@gmail.com>
2025-06-29 21:50:22 -0600

    #

--- js/modulos/firebase-config.js
+++ js/modulos/firebase-config.js
@@ -4,7 +4,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-aut
 
 const firebaseConfig = {
   apiKey: "AIzaSyDPdVE3_R_ZsjBOZfa4bc9sMgBsLoDLlLc",
-  authDomain: "alfomex-f4efa.firebaseapp.com",
+  authDomain: "alfomex.vercel.app",
   projectId: "alfomex-f4efa",
 };
 
